214b7ca2194503780bfaff886d424d8044837153
DaoAnhVu <s3926187@rmit.edu.vn>
2022-12-13 16:21:46 +0700

    add additional field to the database, fix the constructor according to the new structure

--- House/House.cpp
+++ House/House.cpp
@@ -11,15 +11,17 @@ private:
     string listedEnd;
     string occupiedStart;
     string occupiedEnd;
+    string occupierUsername;
     bool isListed;
     double requiredRating;
     int cosumingPoints;
-    vector<int> scores;
+    int numberOfTimeRated;
+    double ratingScore;
     vector<string> comments;
 
 public:
     House() {}
-    House(string location, string description, int consumingPoints)
+    House(string location, string description, int consumingPoints, int numberOfTimeRated, double ratingScore)
     {
         this->location = location;
         this->description = description;
@@ -30,9 +32,11 @@ public:
         this->occupiedEnd = "";
         this->requiredRating = 0;
         this->cosumingPoints = consumingPoints;
+        this->numberOfTimeRated = numberOfTimeRated;
+        this->ratingScore = ratingScore;
     }
 
-    House(string location, string description, bool isListed, string listedStart, string listedEnd, string occupiedStart, string occupiedEnd, double requiredRating, int cosumingPoints)
+    House(string location, string description, bool isListed, string listedStart, string listedEnd, string occupiedStart, string occupiedEnd, string occupierUsername, double requiredRating, int cosumingPoints, int numberOfTimeRated, double ratingScore)
     {
         this->location = location;
         this->description = description;
@@ -43,6 +47,8 @@ public:
         this->occupiedEnd = occupiedEnd;
         this->requiredRating = requiredRating;
         this->cosumingPoints = cosumingPoints;
+        this->numberOfTimeRated = numberOfTimeRated;
+        this->ratingScore = ratingScore;
     }
 
     friend class Member;
